79a25e92fd73cc71536d45a7bc3d59e64845cf34
Hugo Lima <hugohlllima@gmail.com>
2021-10-01 11:55:33 -0300

    criando branch para fazer as mudanças apenas no pc do trabalho

--- conexao.php
+++ conexao.php
@@ -1,6 +1,6 @@
 <?php
 
-$mysqli = new mysqli('mysql', 'root', 'root', 'db_CRUD_1'); 
+$mysqli = new mysqli('localhost', 'root', 'spider18', 'db_CRUD'); 
  if ($mysqli->connect_error) { 
    die("Error connecting: {$mysqli->connect_errno} {$mysqli->connect_error}\n"); 
  } 
